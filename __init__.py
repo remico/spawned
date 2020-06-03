b12bc62cd11d3c7628fa23ca9f13092072079bef
remico <remicollab@gmail.com>
2020-06-03 23:28:30 +0300

    fix imports

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@
 #  software.
 
 from .spawned.spawned import *
+from .spawned.chroot import *
 from .spawned import logger
 
 __author__ = "Roman Gladyshev"
--- spawned/__init__.py
+++ spawned/__init__.py
@@ -14,6 +14,7 @@
 #  software.
 
 from .spawned import *
+from .chroot import *
 from . import logger
 
 __author__ = "Roman Gladyshev"
